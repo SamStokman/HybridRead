83913515863aa4bc07359a9805f8ab95bfd0ae8c
SamStokman <sam.stokman@wur.nl>
2019-08-12 16:02:10 +0200

    docstring added

--- ProcessHybridReads.py
+++ ProcessHybridReads.py
@@ -1,7 +1,8 @@
 """
 10-07-'19
 
-Script that creates the tables with  number of hybrid reads (1 switch) for each allele combo 
+Script that creates the tables with  number of hybrid reads (1 switch) for each allele combo. 
+Use output file hybrid_read_1_switch.txt from SelectHybridReads.py
 
 Command line: python3 ProcessHybridReads.py hybrid_1_switch_data_hla_a.txt hybrid_1_switch_data_hla_b.txt hybrid_1_switch_data_hla_b.txt 
 
@@ -12,7 +13,16 @@ from sys import argv
  
 def create_combo_output(input_file1, input_file2, input_file3):
     """
+    Function that processes three input files (from SelectHybridReads.py) with hybrid read 1 switch data. Per allele
+    combination the number of reads is determined, per file but also for the three file together. If the read is present
+    more than once, then it is ignored. This data is written into a output file.
 
+    Args:
+        input_file1 (str): contains all hybrid reads, and their allele data, that were initially assigned to HLA-A.
+        input_file2 (str): contains all hybrid reads, and their allele data, that were initially assigned to HLA-B.
+        input_file3 (str): contains all hybrid reads, and their allele data, that were initially assigned to HLA-C.
+    Returns:
+        -
     """
    
     collect_all_data = []
@@ -22,7 +32,7 @@ def create_combo_output(input_file1, input_file2, input_file3):
         line = line.split('\t')
         read_name = line[0]
         artefacts = int(line[7])
-        if artefacts == 0:
+        if artefacts == 0:   # change here the number of allowed artefacts for HLA-A
             approved_read_list += [read_name]
         collect_all_data += [['A'] + line]
 
@@ -31,7 +41,7 @@ def create_combo_output(input_file1, input_file2, input_file3):
         line = line.split('\t')
         read_name = line[0]
         artefacts = int(line[7])
-        if artefacts == 0:
+        if artefacts == 0:    # change here the number of allowed artefacts for HLA-B
             approved_read_list += [read_name]
         collect_all_data += [['B'] + line]
   
@@ -40,7 +50,7 @@ def create_combo_output(input_file1, input_file2, input_file3):
         line = line.split('\t')
         read_name = line[0]
         artefacts = int(line[7])
-        if artefacts == 0:
+        if artefacts == 0:   # change here the number of allowed artefacts for HLA-C
             approved_read_list += [read_name]
         collect_all_data += [['C'] + line]
 
@@ -68,20 +78,16 @@ def create_combo_output(input_file1, input_file2, input_file3):
                 continue
   
     read_list_sorted = sorted(read_dict.items(), key=lambda x: x[1])
-
     total_quantity_dict = {}
     for allele, reads in read_list_sorted:
         nr_of_reads = len(reads)
         allele = allele.split(',')
         allele1 = allele[0].strip(' ')
         allele2 = allele[1].strip(' ')
-
         if allele1 in total_quantity_dict.keys():
             total_quantity_dict[allele1] += nr_of_reads
-
         if allele2 in total_quantity_dict.keys():
             total_quantity_dict[allele2] += nr_of_reads
-
         if allele1 not in total_quantity_dict.keys():
             total_quantity_dict[allele1] = nr_of_reads
         if allele2 not in total_quantity_dict.keys():
@@ -90,6 +96,7 @@ def create_combo_output(input_file1, input_file2, input_file3):
         total_quantity_dict[allele1]
         print (allele, '$', len(reads)) 
 
+    # Create output file with read counts, ratios and total read count per allele.
     output_file_name = 'hybrid_read_summary.txt'
     with open(output_file_name, 'w') as db_file: 
         db_file.write('Allele combination\tHLA-A\tHLA-B\tHLA-C\tTotal\n')
